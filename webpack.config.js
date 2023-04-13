06858650c915c8d9822245dedf550c11c9b9a45c
harshsimform <harsh.v@simformsolutions.com>
2023-04-13 11:43:40 +0530

    simple react app setup using webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true,
+  },
+};
